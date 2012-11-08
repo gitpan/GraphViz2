08d6c945fd4510e4f46bed10c6ce5e8ca76175cb
Ron Savage <ron@savage.net.au>
2012-11-08 03:53:05 +0000

    Import of RSAVAGE/GraphViz2-2.06 from CPAN.
    gitpan-cpan-distribution: GraphViz2
    gitpan-cpan-version:      2.06
    gitpan-cpan-path:         RSAVAGE/GraphViz2-2.06.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/GraphViz2.pm
+++ lib/GraphViz2.pm
@@ -31,7 +31,7 @@ fieldhash my %subgraph         => 'subgraph';
 fieldhash my %verbose          => 'verbose';
 fieldhash my %valid_attributes => 'valid_attributes';
 
-our $VERSION = '2.05';
+our $VERSION = '2.06';
 
 # -----------------------------------------------
 
@@ -463,7 +463,7 @@ sub push_subgraph
 	$$scope{subgraph} = {%{$$scope{subgraph} }, %{$arg{subgraph} } };
 
 	$self -> scope -> push($scope);
-	$self -> command -> push(qq|\nsubgraph "$name" {\n|);
+	$self -> command -> push(qq|\nsubgraph "$name"\n{\n|);
 	$self -> default_graph;
 	$self -> default_node;
 	$self -> default_edge;
